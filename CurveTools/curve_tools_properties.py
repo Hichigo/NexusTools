3424be8473c99bed5e74e05e7b05f84280a908a9
Ivan <hichigo91@gmail.com>
2019-06-30 19:34:03 +0300

    added properties file

---
+++ CurveTools/curve_tools_properties.py
@@ -0,0 +1,49 @@
+import bpy
+from bpy.types import PropertyGroup
+from bpy.props import BoolProperty, EnumProperty, PointerProperty
+
+from .curve_tools_utils import filter_on_curve_type
+
+class CurveTools_SCENE_Properties(PropertyGroup):
+
+	target_curve: PointerProperty(
+		name="Target curve",
+		description="Chose curve object",
+		type=bpy.types.Object,
+		poll=filter_on_curve_type
+	)
+
+	follow_curve: BoolProperty(
+		name="Follow curve",
+		description="Object follow by curve",
+		default=False
+	)
+
+	curve_radius: BoolProperty(
+		name="Curve radius",
+		description="Apply radius curve to object",
+		default=False
+	)
+
+	forward_axis: EnumProperty(
+		name="Forward axis",
+		items=[
+			("FORWARD_X", "X", "", 0),
+			("FORWARD_Y", "Y", "", 1),
+			("FORWARD_Z", "Z", "", 2),
+			("TRACK_NEGATIVE_X", "-X", "", 3),
+			("TRACK_NEGATIVE_Y", "-Y", "", 4),
+			("TRACK_NEGATIVE_Z", "-Z", "", 5)
+		],
+		default = "FORWARD_X"
+	)
+
+	up_axis: EnumProperty(
+		name="Up axis",
+		items=[
+			("UP_X", "X", "", 0),
+			("UP_Y", "Y", "", 1),
+			("UP_Z", "Z", "", 2)
+		],
+		default = "UP_Z"
+	)
